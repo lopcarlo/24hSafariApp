370639083eedcd271b667d821a95ff2447368257
ANAlogic <ana.raquel.carlos.carmo@hotmail.com>
2019-04-17 23:33:21 +0100

    creating pack objects

--- js/tours_specific.js
+++ js/tours_specific.js
@@ -1,1 +1,70 @@
- 
+$(document).ready(function () {
+
+    var accommodation_type = ["Tent", "Hotel"]; 
+    var food_type = ["Bugs", ];
+    var flight_type = ["Low Cost", "First Class"];
+    var basic_places_tanzania = [
+        {day: 1,
+         description: "",
+         image: 
+        }
+    ];
+
+var pack_basic_tanzania = {
+    name: "Kuishi Safari",
+    days: 5,
+    accommodation: {
+        type: accommodation_type[0],
+        price: 300
+    },
+    food: food_type[0], 
+    flight: {
+        type: flight_type[0],
+        price: 500 
+    },
+    places:  
+    activities: ,
+    survival_kit: 
+    price: 1000
+}
+
+var pack_premium_tanzania = {
+    name: "Mtafiti Safari",
+    days: 
+    accommodation:
+    price:
+    food: 
+    flight: 
+    animals: 
+    places:
+    activities: 
+    survival_kit:
+}
+
+var pack_basic_quenia = {
+    name: 
+    days: 
+    accommodation:
+    price:
+    food: 
+    flight: 
+    animals: 
+    places:
+    activities: 
+    survival_kit:
+}
+
+var pack_premium_quenia = {
+    name: 
+    days: 
+    accommodation:
+    price:
+    food: 
+    flight: 
+    animals: 
+    places:
+    activities: 
+    survival_kit:
+}
+
+})
