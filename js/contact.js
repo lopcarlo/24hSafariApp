7b43dfc1261e805c682fc31b6604e74e48800ddf
Luis <lopcarlo@gmail.com>
2019-04-18 02:01:05 +0100

    contacts

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,29 @@
- 
+$(document).ready(function () {
+
+    function init() {
+        ccFormAction();
+
+        $("#ccform").submit(function (e) {
+            e.preventDefault();
+        });
+
+    }
+    init();
+
+
+
+    function ccFormAction() {
+        $("#ccbtn").click(function () {
+            console.log("teste");
+            
+
+
+        })
+
+    }
+
+
+
+
+
+});
