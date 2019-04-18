6a2da04ba3c113068ee401a7dcb2134fb48d8ecc
rGuedes <guedes.rodrigo93@gmail.com>
2019-04-18 08:13:15 +0100

    still in packs a little bit more

--- js/pack.js
+++ js/pack.js
@@ -11,10 +11,21 @@ $(document).ready(function() {
     $('#packName').html(currentPack.name);
     $('#packDescriptionText').html(currentPack.description);
 
-    var itenerary = currentPack.itenerary;
+    console.log(currentPack.itinerary);
+    
+    var itinerary = currentPack.itinerary;
+    console.log(itinerary);
+    
+    itinerary.forEach(function(element) {
+        $('#itinerary').append('<div id="day'+ element.day +'" class="dayContainer"> <p>' + element.description + '</p> </div>');
+        console.log(typeof element.image);
+        
+            $('day'+ element.day).append('<div class="dayImage"><img src="' + element.image + '"></div>');
+        
 
-    itenerary.forEach(function(element) {
-        $('itinerary').append('<div id="day'+ element.id +'"></div>')
     });
+
+
+
 
 });
